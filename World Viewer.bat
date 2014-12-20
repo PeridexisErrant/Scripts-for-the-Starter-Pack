47409868d483b4980b11581ba3d6e0c67e5bce0b
Peridexis Errant <PeridexisErrant@gmail.com>
2014-12-20 15:30:26 +1100

    Work correctly from PyLNP launcher
    Some people have problems where .bat scripts started with the root dir
    as %CD%, so we explicitly move to the script location first.

--- World Viewer.bat
+++ World Viewer.bat
@@ -2,7 +2,9 @@
 :: put in World Viewer utilities folder, launches the correct 32/64 bit for your system
 :: then add the executables to the exclusion list to hide from the launcher
 :: adapted from https://support.microsoft.com/kb/556009
- 
+
+cd %~dp0
+
 Set RegQry=HKLM\Hardware\Description\System\CentralProcessor\0
  
 REG.exe Query %RegQry% > checkOS.txt
